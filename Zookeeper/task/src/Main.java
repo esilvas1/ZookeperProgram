30f7ce810aba253f0ae9fa272bf79fa526d8fcc7
Edwin Silva Home <edwinsils@hotmail.com>
2022-12-08 20:30:20 -0500

    Started the proyect Zookeeper - I had developed the stage 1/4

---
+++ Zookeeper/task/src/Main.java
@@ -0,0 +1,17 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        displayMessage();
+    }
+
+    public static void displayMessage() {
+        System.out.printf("I love animals!\n" +
+                "Let's check on the animals...\n" +
+                "The deer looks fine.\n" +
+                "The bat looks happy.\n" +
+                "The lion looks healthy.");
+    }
+}
